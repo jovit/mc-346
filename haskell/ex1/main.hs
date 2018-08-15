01603ff01798fa4a056ff425981f17fa9424de08
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2018-08-15 20:10:03 -0300

    some ex

--- haskell/ex1/main.hs
+++ haskell/ex1/main.hs
@@ -1,2 +1,78 @@
+par y = if (mod y 2) == 0 then True else False
+
 somap [] = 0
 somap (x: xs) = (if par x then x else 0) + somap xs
+
+-- conta ocorrencias de it no vetor
+conta it [] = 0
+conta it (x:xs) 
+    | x == it = 1 + (conta it xs)
+    | otherwise = conta it xs
+
+
+--    tamanho de uma lista
+tam [] = 0
+tam (x:xs) = tam(xs) + 1
+
+--    soma dos elementos de uma lista
+soma [] = 0
+soma (x:xs) = x + soma(xs)
+
+--    soma dos números pares de uma lista
+somap2 [] = 0
+somap2 (x: xs)
+    | par x = x + somap2 xs
+    | otherwise = somap2 xs
+
+--    soma dos elementos nas posições pares da lista ( o primeiro elemento esta na posicao 1)
+somapp [] = 0
+somapp (x:[]) = 0
+somapp (x: xs) = head(xs) + somapp(tail(xs))
+
+--    existe item na lista (True ou False)
+existe it [] = False
+existe it (x:xs)
+    | x == it = True
+    | otherwise = existe it xs
+
+--    posição do item na lista (0 se nao esta la, 1 se é o primeiro)
+posicao it [] = 0
+posicao it (x:xs)
+    | it == x = 1
+    | (existe it xs) = (1 + posicao it xs)
+    | otherwise = 0
+
+
+--    conta quantas vezes o item aparece na lista (0 se nenhuma)
+--    maior elemento de uma lista - FAZER p/ proxima aula - variáveis locais
+--    reverte uma lista - FAZER p/ próxima aula - recursão com acumulados
+--    intercala 2 listas (intercala1 e intercala2)
+--    intercala1 [1,2,3] [4,5,6,7,8]
+--     ==> [1,4,2,5,3,6]
+--    intercala2 [1,2,3] [4,5,6,7,8]
+--     ==>  [1,4,2,5,3,6,7,8]
+--    a lista ja esta ordenada?
+--    dado n gera a lista de 1 a n
+--    retorna o ultimo elemento de uma lista
+--    retorna a lista sem o utlimo elemento
+--    shift right
+--    shiftr [1,2,3,4]
+--     ==> [4,1,2,3]
+shiftr1 [] = []
+shiftr1 (x:[]) = [x]
+shiftr1 (x:xs) = (head(shiftr1 xs):(x:tail(shiftr1 xs)))
+
+--    shiftr n lista (shift right n vezes)
+shiftr 0 (x:xs) = (x:xs)
+shiftr n (x:[]) = [x]
+shiftr n (x:xs) = shiftr (n-1) (head(shiftr 1 xs):(x:tail(shiftr 1 xs)))
+
+--    shift left
+--    shift left n vezes
+--    remove item da lista (1 vez so)
+--    remove item da lista (todas as vezes)
+--    remove item da lista n (as primeiras n vezes)
+--    remove item da lista (a ultima vez que ele aparece) **
+--    troca velho por novo na lista (1 so vez)
+--    troca velho por novo na lista (todas vezes)
+--    troca velho por novo na lista n (as primeiras n vezes)
